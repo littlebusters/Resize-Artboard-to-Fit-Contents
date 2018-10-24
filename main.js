7d012e3d71e4bd11ab790395666d81871851918d
littlebusters <little.busters.reply@gmail.com>
2018-10-24 23:33:46 +0900

    Add settings
    Read / write file, create / show dialog

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const Artboard = require("scenegraph").Artboard;
+const fs = require("uxp").storage.localFileSystem;
+const configFile = 'config.json';
 
-function resizeToFit(selection) {
+console.log('-----');
+
+async function resizeToFit(selection) {
     let sel = selection.items;
+    console.log(1);
+    let config = JSON.parse(await readConfig());
+    console.log(config);
+    console.log(config.width);
 
     for (let selLng = 0; selLng < sel.length; selLng++) {
         let node = sel[selLng];
@@ -30,8 +38,182 @@ function resizeToFit(selection) {
     }
 }
 
+async function resizeToFitPluginSettings() {
+    console.log('Start readConfig');
+    const defaultVal = await readConfig();
+    console.log('createDialog');
+    const dialog = createDialog(defaultVal);
+    try {
+        console.log('showModal');
+        const result = await dialog.showModal();
+        console.log(result);
+        if ('reasonCanceled' !== result) {
+            let config = {}
+            config.width = (checkValue(result.width)) ? Math.abs(result.width - 0) : defaultVal.width;
+            config.height = (checkValue(result.height)) ? Math.abs(result.height - 0) : defaultVal.height;
+            config.offsetBottom = (checkValue(result.offsetBottom)) ? result.offsetBottom - 0 : defaultVal.offsetBottom;
+            await writeConfig(config)
+            console.log(config);
+        }
+    } catch (e) {
+        console.log('Catch');
+        console.log(e);
+    }
+
+}
+
+function checkValue(val) {
+    console.log('-> checkValue')
+    console.log(val);
+    if (isNaN(val - 0)) {
+        console.log('Not number');
+        return false;
+    } else {
+        console.log('isNumber');
+        return true;
+    }
+}
+
+const dom = sel => document.querySelector(sel);
+function createDialog(defautlVal) {
+    console.log('-> Run createDialog')
+    const defaultVal = JSON.parse(defautlVal);
+    console.log(defaultVal);
+
+    document.body.innerHTML = `
+<style>
+    #title {
+        margin-bottom: 16px;
+        font-size: 16px;
+    }
+    h2 {
+        margin-top: 16px;
+    }
+    label {
+        padding-top: 8px;
+    }
+    input {
+        width: 70px;
+    }
+    .formgroup,
+    .container, {
+        display: flex;
+    }
+    .formgroup {
+        padding: 0 8px 8px;
+    }
+    .note {
+        font-size: 12px;
+    }
+</style>
+<dialog id="dialog">
+	<form id="form" method="dialog">
+		<h1 id="title">Resize Artboard to Fit Content Settings</h1>
+		<h2>Fix values</h2>
+		<div class="formgroup">
+            <div class="container">
+                <label for="width">Width</label>
+                <input id="width" type="number" step="1" placeholder="0" value="${defaultVal.width}" />
+            </div>
+            <div class="container">
+                <label for="height">Height</label>
+                <input id="height" type="number" step="1" placeholder="0" value="${defaultVal.height}" />
+            </div>
+        </div>
+		<h2>Offset bounding</h2>
+		<div class="formgroup">
+            <div class="container">
+                <label for="offsetBottom">Bottom</label>
+                <input id="offsetBottom" type="number" step="1" placeholder="0" value="${defaultVal.offsetBottom}" />
+            </div>
+		</div>
+        <p class="note">"0" is follows the size of the content.</p>
+		<footer>
+			<button id="cancel">Cancel</button>
+			<button id="ok" type="submit" uxp-variant="cta">OK</button>
+		</footer>
+	</form>
+</dialog>
+`
+    const dialog = dom('#dialog');
+    const form = dom('#form');
+    const width = dom('#width');
+    const height = dom('#height');
+    const offsetBottom = dom('#offsetBottom');
+    const cancel = dom('#cancel');
+    const ok = dom('#ok');
+
+    cancel.onClick = function() {
+        dialog.close();
+    };
+    ok.onClick = form.onsubmit = function(e) {
+        let config = {};
+        config.width = width.value;
+        config.height = height.value;
+        config.offsetBottom = offsetBottom.value;
+        dialog.close(config);
+        e.preventDefault();
+    };
+
+    return dialog;
+
+}
+
+
+
+async function readConfig() {
+    const pluginDataFolder = await fs.getDataFolder();
+    const entries = await pluginDataFolder.getEntries();
+    let entry;
+    for (let i = 0; i < entries.length; i++) {
+        if (configFile === entries[i].name) {
+            console.log('Config file Found');
+            entry = await entries[i].read();
+            break;
+        }
+    }
+    if (entry) {
+        console.log('Read config file');
+        console.log(entry);
+        return entry;
+    } else {
+        console.log('Config file not found');
+        let defaultVal = {"width": 0, "height": 0, "offsetBottom": 0};
+        const buffer = await pluginDataFolder.createFile(configFile);
+        buffer.write(JSON.stringify(defaultVal));
+
+        return defaultVal;
+    }
+}
+
+async function writeConfig(val) {
+    const pluginDataFolder = await fs.getDataFolder();
+    const entries = await pluginDataFolder.getEntries();
+    let entry;
+    for (let i = 0; i < entries.length; i++) {
+        if (configFile === entries[i].name) {
+            console.log('Config file Found');
+            entry = await entries[i];
+            break;
+        }
+    }
+    if (entry) {
+        console.log('Write config file');
+        entry.write(JSON.stringify(val));
+
+        return true;
+    } else {
+        console.log('Config file not found');
+        const buffer = await pluginDataFolder.createFile(configFile);
+        buffer.write(JSON.stringify(val));
+
+        return true;
+    }
+}
+
 module.exports = {
     commands: {
-        "ResizeToFit": resizeToFit
+        "ResizeToFit": resizeToFit,
+        "ResizeToFitSettings": resizeToFitPluginSettings
     }
 };
