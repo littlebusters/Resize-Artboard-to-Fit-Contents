c6900fdfe9f0b7b2b745900b711c61657feb1ba2
littlebusters <little.busters.reply@gmail.com>
2018-10-25 22:51:22 +0900

    add i18l

--- modules/i18l.js
+++ modules/i18l.js
@@ -1,32 +1,14 @@
-export const i18l = () => {
-    const uiText = {
-        title: {
-            default: 'Resize Artboard to Fit Content Settings',
-            ja: '設定'
-        },
-        labelSize: {
-            default: 'Fix values',
-            ja: '固定サイズ'
-        },
-        labelSizeWidth: {
-            default: 'Width',
-            ja: '幅'
-        },
-        labelSizeHeight: {
-            default: 'Height',
-            ja: '高さ'
-        },
-        labelOffset: {
-            default: 'Offset Bounding',
-            ja: 'オフセット'
-        },
-        labelOffsetBottom: {
-            default: 'Bottom',
-            ja: '下辺'
-        },
-        note: {
-            default: '"0" is follows the size of the content.',
-            ja: '0を入力すると、コンテンツに合わせます。'
-        }
-    }
+let application = require("application");
+
+function getUiLabel(appLang) {
+    if ('en' === appLang) appLang = 'default';
+    let langFilePath = './i18l/' + appLang + '.json';
+    let label = require(langFilePath);
+
+    return label;
+}
+const uiLabel = getUiLabel(application.appLanguage);
+
+module.exports = {
+    uiLabel
 }
