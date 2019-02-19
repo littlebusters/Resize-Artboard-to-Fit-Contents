fdc513e83bd35bcb8b6e0f35223e91bee6503687
littlebusters <little.busters.reply@gmail.com>
2019-02-19 22:39:07 +0900

    Change format

--- modules/helper.js
+++ modules/helper.js
@@ -5,16 +5,17 @@ const {uiLabel} = require('./i18l');
 const pluginIcon = '../images/resize-to-fit-icon.png';
 
 function validateNum(val) {
-    if (isNaN(val - 0)) {
-        return false;
-    } else {
-        return true;
-    }
+	if (isNaN(val - 0)) {
+		return false;
+	}
+	else {
+		return true;
+	}
 }
 
 function createDialog(defaultVal) {
 
-    document.body.innerHTML = `
+	document.body.innerHTML = `
 <style>
     dialog {
         display: flex;
@@ -81,34 +82,34 @@ function createDialog(defaultVal) {
 	<div class="plugin-icon"></div>
 </dialog>
 `;
-    const dialog = dom('#dialog');
-    const form = dom('#form');
-    const width = dom('#width');
-    const height = dom('#height');
-    const offsetBottom = dom('#offsetBottom');
-    const cancel = dom('#cancel');
-    const ok = dom('#ok');
+	const dialog = dom('#dialog');
+	const form = dom('#form');
+	const width = dom('#width');
+	const height = dom('#height');
+	const offsetBottom = dom('#offsetBottom');
+	const cancel = dom('#cancel');
+	const ok = dom('#ok');
 
-    // Cancel button event
-    const cancelDialog = () => dialog.close('reasonCanceled');
-    cancel.addEventListener('click', cancelDialog);
-    cancel.addEventListener('keypress', cancelDialog);
+	// Cancel button event
+	const cancelDialog = () => dialog.close('reasonCanceled');
+	cancel.addEventListener('click', cancelDialog);
+	cancel.addEventListener('keypress', cancelDialog);
 
-    // OK button event
-    const confirmedDialog = (e) => {
-        let config = {};
-        config.width = width.value;
-        config.height = height.value;
-        config.offsetBottom = offsetBottom.value;
-        dialog.close(config);
-        e.preventDefault();
-    };
-    ok.addEventListener('click', confirmedDialog);
-    ok.addEventListener('keypress', confirmedDialog);
+	// OK button event
+	const confirmedDialog = (e) => {
+		let config = {};
+		config.width = width.value;
+		config.height = height.value;
+		config.offsetBottom = offsetBottom.value;
+		dialog.close(config);
+		e.preventDefault();
+	};
+	ok.addEventListener('click', confirmedDialog);
+	ok.addEventListener('keypress', confirmedDialog);
 
-    form.onsubmit = confirmedDialog;
+	form.onsubmit = confirmedDialog;
 
-    return dialog;
+	return dialog;
 
 }
 
@@ -155,38 +156,40 @@ function createAlert(title, msg) {
 }
 
 async function readConfig() {
-    let entry = await openFile();
+	let entry = await openFile();
 
-    if (entry) {
-        console.log('true');
-        return JSON.parse(await entry.read());
-    } else {
-			console.log('false');
-        // Set and return default values if config.json is not found
-        let defaultVal = {"width": 0, "height": 0, "offsetBottom": 0};
-        const pluginDataFolder = await fs.getDataFolder();
-        const buffer = await pluginDataFolder.createFile(configFile);
-        buffer.write(JSON.stringify(defaultVal));
+	if (entry) {
+		console.log('true');
+		return JSON.parse(await entry.read());
+	}
+	else {
+		console.log('false');
+		// Set and return default values if config.json is not found
+		let defaultVal = {"width": 0, "height": 0, "offsetBottom": 0};
+		const pluginDataFolder = await fs.getDataFolder();
+		const buffer = await pluginDataFolder.createFile(configFile);
+		buffer.write(JSON.stringify(defaultVal));
 
-        return defaultVal;
-    }
+		return defaultVal;
+	}
 }
 
 async function writeConfig(val) {
-    let entry = await openFile();
+	let entry = await openFile();
 
-    if (entry) {
-        await entry.write(JSON.stringify(val));
+	if (entry) {
+		await entry.write(JSON.stringify(val));
 
-        return true;
-    } else {
-        // Create file and write value if config.json is not found
-        const pluginDataFolder = await fs.getDataFolder();
-        const buffer = await pluginDataFolder.createFile(configFile);
-        buffer.write(JSON.stringify(val));
+		return true;
+	}
+	else {
+		// Create file and write value if config.json is not found
+		const pluginDataFolder = await fs.getDataFolder();
+		const buffer = await pluginDataFolder.createFile(configFile);
+		buffer.write(JSON.stringify(val));
 
-        return true;
-    }
+		return true;
+	}
 }
 
 async function openFile() {
@@ -204,9 +207,9 @@ async function openFile() {
 }
 
 module.exports = {
-    validateNum,
-    createDialog,
-    readConfig,
-    writeConfig,
-    createAlert
+	validateNum,
+	createDialog,
+	readConfig,
+	writeConfig,
+	createAlert
 }
